e6cf293c298d3b2a05515029b143862731b433bf
Alexisfllv <alexis-fl-live@hotmail.com>
2024-08-21 19:01:02 -0500

    base

---
+++ src/main/java/com/example/AeosS03RestwsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeosS03RestwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeosS03RestwsApplication.class, args);
+	}
+
+}
